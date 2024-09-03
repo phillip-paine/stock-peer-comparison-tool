fa3bfacb979146a051140da3c11242102022c83f
Phillip Paine <pmxpp88@gmail.com>
2024-09-02 17:43:55 -0700

    scenario forecasts
    add the scenario forecasting to the table and plots

--- src/peer-comparison-tool/comparison_tool/discounted_cashflow_model_page.py
+++ src/peer-comparison-tool/comparison_tool/discounted_cashflow_model_page.py
@@ -71,14 +71,6 @@ def get_discounted_cashflow_model_page_layout(data, latest_ev_data):
             dbc.Col([
                 html.H5("Discounted Cash Flow:"),
 
-                dbc.Card([
-                    dbc.CardHeader("Calculated DCF Value"),
-                    dbc.CardBody([
-                        html.H4(id='dcf-result', className='card-title'),
-                        html.P("This is the present value of projected cash flows", className='card-text')
-                    ]),
-                ]),
-
                 # EV Value
                 dbc.Card([
                     dbc.CardHeader("Enterprise Value (EV)"),
@@ -86,16 +78,31 @@ def get_discounted_cashflow_model_page_layout(data, latest_ev_data):
                         html.H4(id='ev-value', className='card-title'),
                         html.P("Current market-based value of the company", className='card-text')
                     ]),
-                ]),
+                ], style={'background-color': colors['background'], 'margin-bottom': '10px'}),
 
-                # Percentage Error
+                # Percentage Error for each scenario
                 dbc.Card([
-                    dbc.CardHeader("Percentage Error"),
+                    dbc.CardHeader("Scenario Percentage Error"),
                     dbc.CardBody([
-                        html.H4(id='percentage-error', className='card-title'),
-                        html.P("Error between DCF value and EV", className='card-text')
+                        # Low scenario percentage error
+                        dbc.Row([
+                            dbc.Col(html.H5("Low (Bear):"), width=4),
+                            dbc.Col(html.H5(id="bear-percentage-error", style={"color": "red"}), width=8),
+                        ], className="mb-2"),
+
+                        # Mid scenario percentage error
+                        dbc.Row([
+                            dbc.Col(html.H5("Mid (Base):"), width=4),
+                            dbc.Col(html.H5(id="base-percentage-error", style={"color": "white"}), width=8),
+                        ], className="mb-2"),
+
+                        # High scenario percentage error
+                        dbc.Row([
+                            dbc.Col(html.H5("High (Bull):"), width=4),
+                            dbc.Col(html.H5(id="bull-percentage-error", style={"color": "green"}), width=8),
+                        ]),
                     ]),
-                ]),
+                ], style={'background-color': colors['background'], 'margin-bottom': '10px'})
             ], width=4)
         ])
 
@@ -104,9 +111,10 @@ def get_discounted_cashflow_model_page_layout(data, latest_ev_data):
 
 def register_discounted_cashflow_model_page_callbacks(app, cashflow_map, latest_ev):
     @app.callback(
-        dash.dependencies.Output('dcf-result', 'children'),
         dash.dependencies.Output('ev-value', 'children'),
-        dash.dependencies.Output('percentage-error', 'children'),
+        dash.dependencies.Output('base-percentage-error', 'children'),
+        dash.dependencies.Output('bear-percentage-error', 'children'),
+        dash.dependencies.Output('bull-percentage-error', 'children'),
         dash.dependencies.Output("cash-flow-growth-plot", "figure"),
         dash.dependencies.Input('calculate-dcf-button', 'n_clicks'),
         dash.dependencies.State('company-dropdown', 'value'),
@@ -123,36 +131,59 @@ def register_discounted_cashflow_model_page_callbacks(app, cashflow_map, latest_
         if df_cashflow is None:
             return ""
 
+        # Now we want the three scenarios:
         # Example calculation (simplified)
-        latest_free_cashflow = df_cashflow['Free Cash Flow'].iloc[-1]  # we want the latest value as ordered
-        future_cashflows = []
-        discounted_future_cashflows = []
-        for year in range(1, CF_FUTURE_YEARS + 1):
-            future_cashflows.append(latest_free_cashflow*((1 + growth_rate/100) ** year))
-            discounted_future_cashflows.append(future_cashflows[-1] / ((1 + discount_rate / 100) ** year))
-        terminal_value = future_cashflows[-1] * (1 + terminal_rate / 100) / (discount_rate - terminal_rate)
-        discounted_terminal_value = terminal_value / ((1 + discount_rate / 100) ** CF_FUTURE_YEARS)
-
-        # Cash flow ts: add the forecast cashflow to the historical cashflows
         start_date = df_cashflow['date'].iloc[-1] + pd.DateOffset(years=1)
-        df_future_cashflow = pd.DataFrame({"date": pd.date_range(start_date.strftime("%Y-%m-%d"), periods=CF_FUTURE_YEARS, freq='Y'),
-                                           "Discounted Forecast Free Cash Flow": discounted_future_cashflows})
-        df_ts_plot = pd.concat([df_cashflow[['date', 'Free Cash Flow']], df_future_cashflow])
+        latest_free_cashflow = df_cashflow['Free Cash Flow'].iloc[-1]  # we want the latest value as ordered
+
+        df_forecast_cf, forecast_discounted_cf, forecast_discounted_terminal_value = \
+            create_forecast_cashflow_dataframe(latest_free_cashflow, growth_rate, discount_rate, terminal_rate, start_date)
+        df_bear_forecast_cf, low_forecast_discounted_cf, low_forecast_discounted_terminal_value = \
+            create_forecast_cashflow_dataframe(latest_free_cashflow, growth_rate*0.8, discount_rate*1.1, terminal_rate*0.8, start_date)
+        df_bear_forecast_cf.rename(columns={'Discounted Forecast Free Cash Flow': 'Bear Discounted Forecast FCF'}, inplace=True)
+        df_bull_forecast_cf, high_forecast_discounted_cf, high_forecast_discounted_terminal_value = \
+            create_forecast_cashflow_dataframe(latest_free_cashflow, growth_rate*1.2, discount_rate*0.9, terminal_rate*1.2, start_date)
+        df_bull_forecast_cf.rename(columns={'Discounted Forecast Free Cash Flow': 'Bull Discounted Forecast FCF'}, inplace=True)
+
+        df_ts_plot = pd.concat([df_cashflow[['date', 'Free Cash Flow']], df_forecast_cf,
+                                df_bear_forecast_cf, df_bull_forecast_cf])
 
         df_ts_plot['year'] = df_ts_plot['date'].dt.year.astype(str)
-        df_ts_plot = df_ts_plot.melt(id_vars='year', value_vars=['Free Cash Flow', 'Discounted Forecast Free Cash Flow'],
+        df_ts_plot = df_ts_plot.melt(id_vars='year', value_vars=['Free Cash Flow', 'Discounted Forecast Free Cash Flow',
+                                                                 'Bear Discounted Forecast FCF', 'Bull Discounted Forecast FCF'],
                                      var_name='Cash Flow Type', value_name='Value')
         # Now lets add the calculated free cash flow values forecast:
         cashflow_ts_plot = px.line(df_ts_plot, x='year', y='Value', color='Cash Flow Type',
                                    title="Historical Cash Flow and Discounted Forecast Cash Flow Growth Over Time")
 
         # And calculate the DCF:
-        dcf_value = int(sum(discounted_future_cashflows) + discounted_terminal_value)
-        dcf_value_str = f"{int((sum(discounted_future_cashflows) + discounted_terminal_value) / 1_000_000)} mm"
+        base_dcf_value = int(forecast_discounted_cf + forecast_discounted_terminal_value)
+        low_dcf_value = int(low_forecast_discounted_cf + forecast_discounted_terminal_value)
+        high_dcf_value = int(high_forecast_discounted_cf + forecast_discounted_terminal_value)
+        # dcf_value_str = f"{int((forecast_discounted_cf + forecast_discounted_terminal_value) / 1_000_000)} mm"
 
         ev_value_str = f"{int(latest_ev.loc[ticker, 'enterprise_value'] / 1_000_000)} mm"
         ev_value = latest_ev.loc[ticker, 'enterprise_value']
 
-        percentage_error = round((dcf_value - ev_value) / ev_value * 100, 2)
+        base_percentage_error = round((base_dcf_value - ev_value) / ev_value * 100, 2)
+        low_percentage_error = round((low_dcf_value - ev_value) / ev_value * 100, 2)
+        high_percentage_error = round((high_dcf_value - ev_value) / ev_value * 100, 2)
 
-        return dcf_value_str, ev_value_str, percentage_error, cashflow_ts_plot
+        return ev_value_str, base_percentage_error, low_percentage_error, high_percentage_error, cashflow_ts_plot
+
+
+def create_forecast_cashflow_dataframe(latest_free_cashflow, growth_rate, discount_rate, terminal_rate, calculation_date):
+    future_cashflows = []
+    discounted_future_cashflows = []
+    for year in range(1, CF_FUTURE_YEARS + 1):
+        future_cashflows.append(latest_free_cashflow*((1 + growth_rate/100) ** year))
+        discounted_future_cashflows.append(future_cashflows[-1] / ((1 + discount_rate / 100) ** year))
+    terminal_value = future_cashflows[-1] * (1 + terminal_rate / 100) / (discount_rate - terminal_rate)
+    discounted_terminal_value = terminal_value / ((1 + discount_rate / 100) ** CF_FUTURE_YEARS)
+    total_discounted_future_cashflows = sum(discounted_future_cashflows)
+
+    # Cash flow ts: add the forecast cashflow to the historical cashflows
+
+    df_future_cashflow = pd.DataFrame({"date": pd.date_range(calculation_date.strftime("%Y-%m-%d"), periods=CF_FUTURE_YEARS, freq='Y'),
+                                       "Discounted Forecast Free Cash Flow": discounted_future_cashflows})
+    return df_future_cashflow, total_discounted_future_cashflows, discounted_terminal_value
